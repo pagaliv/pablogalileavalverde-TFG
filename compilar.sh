ef027d9152d1a2424074f00ee7dc77f23a8401cb
Pablo <pgalilea@arsys.es>
2026-03-09 10:48:04 +0100

    cambio forma de compilar trabajo

--- compilar.sh
+++ compilar.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 echo "Compilando TFG..."
 
-pdflatex main.tex
-biber main
-pdflatex main.tex
-pdflatex main.tex
+pdflatex -output-directory=auxiliares main.tex
+biber --output-directory auxiliares main
+pdflatex -output-directory=auxiliares main.tex
+pdflatex -output-directory=auxiliares main.tex
+
+# Mover el PDF generado al directorio principal
+mv auxiliares/main.pdf main.pdf
 
 echo "¡Listo! Abriendo PDF..."
 xdg-open main.pdf
